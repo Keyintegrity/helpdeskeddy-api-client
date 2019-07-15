3da604c11bf1b53d54aca44bc81f08276ea8b9d4
devxplorer <devxplorer@gmail.com>
2019-07-15 10:48:25 +0300

    upd version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="helpdeskeddy-api-client",
-    version="0.0.1",
+    version="0.0.2",
     author="devxplorer",
     author_email="devxplorer@gmail.com",
     description="Simple HelpDeskEddy api client",
